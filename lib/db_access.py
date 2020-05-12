b4e82dd59a3d1ee34a47744e27b54f0f521c31ed
ry-i-system <ryo.yasumoto@ry-i-system.com>
2020-05-13 08:47:07 +0900

    commit

--- lib/db_access.py
+++ lib/db_access.py
@@ -5,7 +5,6 @@
 
 import sys
 import os
-import json
 import MySQLdb
 
 # 親ディレクトリをアプリケーションのホーム(${app_home})に設定
@@ -19,87 +18,61 @@ from conf.config import DatabaseConf as DC
 class DbAccess(object):
     # DB接続（参照系）
     def dbSelect(sql):
-        try:
-            # DB接続
-            connection = MySQLdb.connect(
-                user=DC.userId,
-                passwd=DC.passWord,
-                host=DC.hostName,
-                db=DC.dbName,
-                charset='utf8',
-                cursorclass=pymysql.cursors.DictCursor)
-            cursor = connection.cursor()
+        # DB接続
+        connection = MySQLdb.connect(
+            user=DC.userId,
+            passwd=DC.passWord,
+            host=DC.hostName,
+            db=DC.dbName,
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor)
+        cursor = connection.cursor()
 
-            # SQL実行
-            cursor.execute(sql)
+        # SQL実行
+        cursor.execute(sql)
 
-            # 結果を取り出す
-            data = cursor.fetchall()
+        # 結果を取り出す
+        data = cursor.fetchall()
 
-            # DB切断
-            cursor.close()
-            connection.close()
-            return data
-
-        except ZeroDivisionError as e:
-            # DB切断
-            cursor.close()
-            connection.close()
-            print('catch ZeroDivisionError:', e)
-            return False
+        # DB切断
+        cursor.close()
+        connection.close()
+        return data
 
     # DB接続（更新系）
     def dbAccess(sql, val):
-        try:
-            # DB接続
-            connection = MySQLdb.connect(
-                user=DC.userId,
-                passwd=DC.passWord,
-                host=DC.hostName,
-                db=DC.dbName)
-            cursor = connection.cursor()
+        # DB接続
+        connection = MySQLdb.connect(
+            user=DC.userId,
+            passwd=DC.passWord,
+            host=DC.hostName,
+            db=DC.dbName)
+        cursor = connection.cursor()
 
-            # SQL
-            cursor.execute(sql,val)
+        # SQL
+        cursor.execute(sql,val)
 
-            # DB切断
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return True
-
-        except ZeroDivisionError as e:
-            # DB切断
-            connection.rollback()
-            cursor.close()
-            connection.close()
-            print('catch ZeroDivisionError:', e)
-            return False
+        # DB切断
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return "Success"
 
     # DBテーブル作成／定義変更
     def dbMigrate(sql):
-        try:
-            # DB接続
-            connection = MySQLdb.connect(
-                user=DC.userId,
-                passwd=DC.passWord,
-                host=DC.hostName,
-                db=DC.dbName)
-            cursor = connection.cursor()
+        # DB接続
+        connection = MySQLdb.connect(
+            user=DC.userId,
+            passwd=DC.passWord,
+            host=DC.hostName,
+            db=DC.dbName)
+        cursor = connection.cursor()
 
-            # SQL
-            cursor.execute(sql)
+        # SQL
+        cursor.execute(sql)
 
-            # DB切断
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return "Success"
-
-        except ZeroDivisionError as e:
-            # DB切断
-            connection.rollback()
-            cursor.close()
-            connection.close()
-            print('catch ZeroDivisionError:', e)
-            return "Failure"
+        # DB切断
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return "Success"
